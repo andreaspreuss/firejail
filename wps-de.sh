6c27024e9bb610fdc592a867860f02897e35533b
Andreas Preuss <andreaspreuss@users.noreply.github.com>
2020-03-02 15:19:17 +0100

    Update wps-de.sh

--- wps-de.sh
+++ wps-de.sh
@@ -10,7 +10,7 @@ apt-get install -f
 # install german language
 apt-get install -y p7zip-full
 wget https://github.com/andreaspreuss/firejail/raw/master/opt/kingsoft/wps-office/office6/mui/de_DE.7z
-7za x de_DE.7z -o/opt/kingsoft/wps-office/office6/mui
+7za x de_DE.7z -aoa -o/opt/kingsoft/wps-office/office6/mui
 # install ms fonts
 # mkdir /usr/share/fonts/wps-fonts
 wget -N https://raw.githubusercontent.com/andreaspreuss/firejail/master/usr/share/fonts/wps-fonts/WEBDINGS.TTF -P /usr/share/fonts/wps-fonts
@@ -26,8 +26,8 @@ mkdir -p /home/$USER/.cache/Kingsoft
 wget -N https://raw.githubusercontent.com/andreaspreuss/firejail/master/home/username/.config/Kingsoft/Office.conf -P /home/$USER/.config/Kingsoft/
 chmod 644 /home/$USER/.config/Kingsoft/*
 # delete unnecassary directories
-rm -r /home/$USER/.kingsoft
-rm -r /home/$USER/模板
+rm -rf /home/$USER/.kingsoft
+rm -rf /home/$USER/模板
 # install firejail
 apt-get install -y firejail firejail-profiles firetools
 wget -N https://raw.githubusercontent.com/andreaspreuss/firejail/master/etc/firejail/wps.profile -P /etc/firejail/
@@ -43,8 +43,8 @@ firecfg
 # ln -s /usr/bin/wpp /usr/local/bin/wpp
 # ln -s /usr/bin/wpspdf /usr/local/bin/wpspdf
 # delete unnecessay files
-rm de_DE.7z 
-rm wps-office_11.1.0.9080.XA_amd64.deb
+rm -f de_DE.7z 
+rm -f wps-office_11.1.0.9080.XA_amd64.deb
 apt-get remove -y p7zip-full 
 apt-get autoclean
 apt-get autoremove
