55cf15295a3e391e678f9d4e18853f7b9860efec
Andreas Preuss <andreaspreuss@users.noreply.github.com>
2020-03-09 15:04:58 +0100

    Update fo-de.sh

--- fo-de.sh
+++ fo-de.sh
@@ -1,22 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 echo "Installation von Freeoffice"
 wget -c https://www.softmaker.net/down/softmaker-freeoffice-2018_974-01_amd64.deb
 dpkg -i softmaker-freeoffice-2018_974-01_amd64.deb
-apt-get install -y qpdfview
+apt-get -y install qpdfview
 # install firejail
 apt-get install -y firejail firejail-profiles firetools
 wget -c https://raw.githubusercontent.com/andreaspreuss/firejail/master/etc/firejail/softmaker-common.inc -P /etc/firejail/
-wget -c https://raw.githubusercontent.com/andreaspreuss/firejail/master/etc/firejail/textmaker18free.profile -P /etc/firejail/
-wget -c https://raw.githubusercontent.com/andreaspreuss/firejail/master/etc/firejail/presentations18free.profile -P /etc/firejail/
-wget -c https://raw.githubusercontent.com/andreaspreuss/firejail/master/etc/firejail/planmaker18free.profile -P /etc/firejail/
-echo "textmaker18free" >> /usr/lib/x86_64-linux-gnu/firejail/firecfg.config
-echo "presentations18free" >> /usr/lib/x86_64-linux-gnu/firejail/firecfg.config
-echo "planmaker18free" >> /usr/lib/x86_64-linux-gnu/firejail/firecfg.config
+wget -c https://raw.githubusercontent.com/andreaspreuss/firejail/master/etc/firejail/textmaker.profile -P /etc/firejail/
+wget -c https://raw.githubusercontent.com/andreaspreuss/firejail/master/etc/firejail/presentations.profile -P /etc/firejail/
+wget -c https://raw.githubusercontent.com/andreaspreuss/firejail/master/etc/firejail/planmaker.profile -P /etc/firejail/
 echo "textmaker" >> /usr/lib/x86_64-linux-gnu/firejail/firecfg.config
+echo "presentations" >> /usr/lib/x86_64-linux-gnu/firejail/firecfg.config
+echo "planmaker" >> /usr/lib/x86_64-linux-gnu/firejail/firecfg.config
+# creating symlinks
+ln -s /usr/share/freeoffice2018/textmaker /usr/bin/textmaker
+ln -s /usr/share/freeoffice2018/planmaker /usr/bin/planmaker
+ln -s /usr/share/freeoffice2018/presentations /usr/bin/presentations
 # echo "$USER" >> /etc/firejail/firejail.users
 # firejail initialisation desktop Integration
 firecfg
 # delete unused files
 rm -rf softmaker-freeoffice-2018_974-01_amd64.deb
 apt-get -y autoremove
-apt-get clean all
+apt-get -y clean all
