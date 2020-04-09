30544abe662364304a1ee08293bf0f311c3de87d
Andreas Preuss <andreaspreuss@users.noreply.github.com>
2020-04-09 09:25:33 +0200

    Create nob.sh

---
+++ opt/nob.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+notify-send 'Verboten !' 'Funktion ist auf diesem System nicht erlaubt.' --icon=dialog-information 
+# zenity --error --text="Funktion ist auf diesem System nicht erlaubt." --title="Verboten\!"
